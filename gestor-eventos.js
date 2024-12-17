21d739e2358e85a769155bbbaba71196f7446a1f
Fran-PR <franporraslago@gmail.com>
2024-12-17 13:32:25 +0100

    Update gestor-eventos.js

--- gestor-eventos.js
+++ gestor-eventos.js
@@ -1,7 +1,29 @@
-
+//busqueda por Titulo
 document.querySelector("#btnTitle").addEventListener("click",()=>{
     clearCards()
     const textoBusqueda=document.querySelector("#txtTitle").value;
+    const peliculasFiltradas=peliculas.filter(
+        pelicula=>pelicula.Title
+        .toUppeCase()
+        .includes(textoBusqueda)
+        .trim()
+        .toUpperCase());
+
+    console.log(peliculasFiltradas);
+
+    peliculasFiltradas.forEach(pelicula => {
+        generateCard(pelicula);
+    });
+
+
+
+})
+
+//busqueda por actor
+document.querySelector("#btnActor").addEventListener("click",()=>
+    {
+    clearCards();
+    const textoBusqueda=document.querySelector("#txtActor").value;
 
     const peliculasFiltradas=peliculas.filter(
         pelicula=>pelicula.Title
@@ -14,10 +36,16 @@ document.querySelector("#btnTitle").addEventListener("click",()=>{
 
     peliculasFiltradas.forEach(pelicula => {
         generateCard(pelicula);
-
     });
 
 
-    //busqueda por actor, genero y orden
 
-})
+
+
+
+    })
+
+//busqueda por genero
+
+//activar orden ascedente y descendente
+
