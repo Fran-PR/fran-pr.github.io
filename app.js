ee93b26c34a9626a62ed9d19e20148261232412c
Fran-PR <franporraslago@gmail.com>
2024-12-17 09:56:40 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const URL='https://fran-pr.github.io/movies-250.json';
 
 function processMovie(data) {
-    console.log(data);
+    const peliculas=data.movies;
+    peliculas.array.forEach(element => {
+        console.log(pelicula.Title);
+    });
 }
 //esta funcion viene de myhttp.js
 doGetRequest(URL, processMovie);
