3589c50bac38b3847d1389e79ec806eaa6cd5333
Fran-PR <franporraslago@gmail.com>
2024-12-17 10:43:46 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const URL='https://fran-pr.github.io/movies-250.json';
 
 //funcion para generar fichas nuevas
 function generateCard(pelicula){
+    //Creacion de la card
     const newCard= document.createElement('div');//esto nos cre un elemento de tipo div
-    document.querySelector("#divFichas").appendChild(newCard);//le agregamos el div al contenedor
+    //Img
+    const newImg =document.createElement("img");//creamos elemento img
+    newImg.setAttribute("src", peliculaPoster);//le añadimos el atributo src
+    newImg.setAttribute("alt", `Poster de la pelicula ${pelicula.Title}`);
+    newCard.appendChild(newImg);//agregamos la imagen a la card
     
+    document.querySelector("#divFichas").appendChild(newCard);//le agregamos el div al contenedor
+
 }
 
 function processMovie(data) {
