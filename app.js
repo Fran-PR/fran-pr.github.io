4d50b65469cdd5074eeb4e47dba38c4c26200b5c
Fran-PR <franporraslago@gmail.com>
2024-12-17 10:07:17 +0100

    Inclusión de for

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const URL='https://fran-pr.github.io/movies-250.json';
 
 function processMovie(data) {
     const peliculas=data.movies;
+    //Recorremos con bucle for tradicional
+    for(let i=0; i<peliculas.length;i++){
+        console.log("Duration:"+peliculas[i].Runtime);
+
+    }
+
+    //Recorremos 
     peliculas.forEach(pelicula => {
         console.log(pelicula.Title);
     });
