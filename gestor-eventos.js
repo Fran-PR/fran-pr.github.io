aa95bff06c7475b23abb28ae17e791e4f978352e
Fran-PR <franporraslago@gmail.com>
2024-12-17 13:56:37 +0100

    Update gestor-eventos.js

--- gestor-eventos.js
+++ gestor-eventos.js
@@ -1,16 +1,12 @@
 //busqueda por Titulo
-document.querySelector("#btnTitle").addEventListener("click",()=>{
+document.querySelector("#btnTitle").addEventListener("click", () => {
     clearCards()
-    const textoBusqueda=document.querySelector("#txtTitle").value;
-    const peliculasFiltradas=peliculas.filter(
-        pelicula=>pelicula.Title
-        .toUppeCase()
-        .includes(textoBusqueda)
-        .trim()
-        .toUpperCase());
-
-    console.log(peliculasFiltradas);
-
+    const textoBusqueda = document.querySelector("#txtTitle").value;
+    const peliculasFiltradas = peliculas.filter(
+        pelicula => pelicula.Title
+            .toUpperCase()
+            .includes(textoBusqueda.trim().toUpperCase())
+    );
     peliculasFiltradas.forEach(pelicula => {
         generateCard(pelicula);
     });
@@ -20,32 +16,41 @@ document.querySelector("#btnTitle").addEventListener("click",()=>{
 })
 
 //busqueda por actor
-document.querySelector("#btnActor").addEventListener("click",()=>
-    {
+document.querySelector("#btnActor").addEventListener("click", () => {
     clearCards();
-    const textoBusqueda=document.querySelector("#txtActor").value;
+    const textoBusqueda = document.querySelector("#txtActor").value;
 
-    const peliculasFiltradas=peliculas.filter(
-        pelicula=>pelicula.Title
-        .toUppeCase()
-        .includes(textoBusqueda)
-        .trim()
-        .toUpperCase());
-
-    console.log(peliculasFiltradas);
-
+    const peliculasFiltradas = peliculas.filter(
+        pelicula => pelicula.Actors
+            .toUpperCase()
+            .includes(textoBusqueda.trim().toUpperCase())
+    );
     peliculasFiltradas.forEach(pelicula => {
         generateCard(pelicula);
     });
 
-
-
-
-
-
-    })
+})
 
 //busqueda por genero
+document.querySelector("#btnGenre").addEventListener("click", () => {
+    const selectElement = document.getElementById("selGenre");
+
+    selectElement.addEventListener('change', (event) => {
+        const selectedValue = event.target.value;
+        console.log('Valor seleccionado:', selectedValue);
+
+        // Aquí puedes hacer algo con el valor seleccionado
+    });
+
+    const peliculasFiltradas = peliculas.filter(
+        pelicula => pelicula.Genre
+            .toUpperCase()
+            .includes(selectedValue.trim().toUpperCase())
+    );
+    peliculasFiltradas.forEach(pelicula => {
+        generateCard(pelicula);
+    });
+})
 
 //activar orden ascedente y descendente
 
